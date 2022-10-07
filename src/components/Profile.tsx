6ad49e8eb288ea2cf4875b15cc9bbf05b6007b10
Guillaume <guillaume@gojob.com>
2022-10-07 16:28:46 +0200

    feat: cleaning cards

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,21 +3,19 @@ import guillaumeOdile from "../images/guillaumeOdile.png";
 
 const Profile = () => {
   return (
-    <div className="rounded-3xl w-full bg-slate-900 bg-opacity-70">
-      <div className="justify-center items-center text-center">
-        <div className="inline-flex flex-col text-left m-8">
-          <span className=" text-gray-200 text-4xl">Guillaume Odile</span>
-          <span className="text-gray-300 text-lg">
-            Développeur full-stack indépendant
-          </span>
-        </div>
-
-        <img
-          src={guillaumeOdile}
-          alt={"Guillaume Odile"}
-          className="inline-block items-center justify-center object-cover aspect-square rounded-full w-32 h-32 m-8 border-2 border-emerald-300"
-        />
+    <div className="rounded-3xl w-full bg-slate-900 bg-opacity-70 justify-center items-center text-center">
+      <div className="inline-flex flex-col text-left m-8">
+        <span className=" text-gray-200 text-4xl">Guillaume Odile</span>
+        <span className="text-gray-300 text-lg">
+          Développeur full-stack indépendant
+        </span>
       </div>
+
+      <img
+        src={guillaumeOdile}
+        alt={"Guillaume Odile"}
+        className="inline-block items-center justify-center object-cover aspect-square rounded-full w-32 h-32 m-8 border-2 border-emerald-300"
+      />
     </div>
   );
 };
