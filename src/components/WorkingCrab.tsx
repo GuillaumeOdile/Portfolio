17eed48a449ebb3c6d0ed10577cfd20a3b173fa4
Guillaume <guillaume@gojob.com>
2022-10-11 16:19:28 +0200

    feat: add crab rotation

--- src/components/WorkingCrab.tsx
+++ src/components/WorkingCrab.tsx
@@ -1,30 +1,44 @@
-import { Canvas } from "@react-three/fiber";
-import React, { useEffect, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useState } from "react";
 import CrabModel from "../Models/CrabModel";
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+
+const CrabMesh = () => {
+  const refMesh = React.useRef() as any;
+  const [scrollY, setScrollY] = useState(window.scrollY);
+  const [lastRotation, setLastRotation] = useState(1);
+
+  useFrame(() => {
+    if (refMesh.current) {
+      if (scrollY > window.scrollY) {
+        refMesh.current.rotation.y += (scrollY - window.scrollY) / 500;
+        setScrollY(scrollY - (scrollY - window.scrollY) / 4);
+        setLastRotation(-1);
+      } else if (scrollY < window.scrollY - 1) {
+        refMesh.current.rotation.y -= (window.scrollY - scrollY) / 500;
+        setScrollY(window.scrollY - (window.scrollY - scrollY) / 4);
+        setLastRotation(1);
+      }
+      refMesh.current.rotation.y -= 0.003 * lastRotation;
+    }
+  });
+
+  return (
+    <mesh ref={refMesh}>
+      <CrabModel />
+    </mesh>
+  );
+};
 
 const WorkingCrab = () => {
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setReady(true), 3000);
-  }, []);
-
-  return ready ? (
+  return (
     <Canvas
       camera={{ position: [150, 50, 50], fov: 15 }}
       className="bg-zinc-700"
       shadows
     >
       <ambientLight intensity={0.9} />
-      <CrabModel />
+      <CrabMesh />
     </Canvas>
-  ) : (
-    <ClimbingBoxLoader
-      size={30}
-      color={"#36D6B6"}
-      className="flex justify-center m-auto"
-    />
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,6 @@ const Home = (props: HomeProps) => {
         <Profile />
         <CareerPath />
         <MyArticles />
-        <FavoriteStack />
       </div>
     </div>
   );
